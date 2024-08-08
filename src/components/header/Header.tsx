05c7604d15887260ea295efb6b63ace08923268f
Amanda Jauregui <amandajauregui.dev@gmail.com>
2024-08-08 13:42:59 -0300

    header finished

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,41 @@
+import handleClearToken from '../../hooks/useAuth'
+
 type IProps = {
   isLogged: boolean;
   name?: string
 }
 
 export default function Header({isLogged, name}: IProps) {
+
+  function handleOpenModal() {
+    //todo
+  }
+
+  function handleLogout() {
+    handleClearToken()
+  }
+
   return (
-    <header className="header">
+    <header className="header" style={{ justifyContent: isLogged ? 'space-between' : 'start' }}>
       <img src="/Logo.png" alt="logo dindin" />  
 
       {
       isLogged && (
         <section>
-          <div>
-            <img src="/1814089_account_user_person_profile_avatar_icon 1.png" alt="conta" />
-            <p>{name}</p>
-            <img src="/sign-out.png" alt="sair" />
+          <div className="login-icons">
+          <img
+            onClick={handleOpenModal}
+            className="user-icon"
+            src="/1814089_account_user_person_profile_avatar_icon 1.png"
+            alt="conta" 
+          />
+              <p className="user-name">{name ? name : "Amanda"}</p>
+            <img 
+              onClick={handleLogout}
+              className="logout"
+              src="/sign-out.png"
+              alt="sair"
+            />
           </div>
         </section>
       ) 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>bem vindo a mia home</h1>
+    <div className="home">
+      <div className="gradiente"></div>
+      <div className="main-background">
+        <div className="container dashboard">
+          <h1>bem vindo a mia home</h1>
+        </div>
+      </div>
     </div>
   )
 }
